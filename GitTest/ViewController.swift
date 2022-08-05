a87d11e100f401bd0f6a967338d3524bc6157919
tugrul <tugakca@gmail.com>
2022-08-05 10:33:25 +0300

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Tugrul Akcadag on 5.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
